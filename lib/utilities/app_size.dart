a020404302aa89c21ca106b02a67a6c3d78fc3af
Taufiq Ahommed Neloy <taufiqneloy.swe@gmail.com>
2024-07-12 03:26:21 +0600

    app size class added

---
+++ lib/utilities/app_size.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/cupertino.dart';
+
+class AppSize {
+  static const SizedBox gapH15 = SizedBox(height: 15);
+  static const SizedBox gapH20 = SizedBox(height: 20);
+  static const SizedBox gapH50 = SizedBox(height: 50);
+}
