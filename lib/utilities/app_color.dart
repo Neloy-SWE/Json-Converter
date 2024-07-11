6884c1a936185e894008f7ee8d63ddc8b8984804
Taufiq Ahommed Neloy <taufiqneloy.swe@gmail.com>
2024-07-12 03:26:34 +0600

    app color class added

---
+++ lib/utilities/app_color.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class AppColor{
+  static const Color colorBackground = Color.fromRGBO(216, 200, 200, 1.0);
+  static const Color colorPrimary = Color.fromRGBO(43, 0, 0, 1.0);
+}
