1918c2044c2ee6fc9249990ee08e6862ce3e7c40
Taufiq Ahommed Neloy <taufiqneloy.swe@gmail.com>
2024-07-12 03:26:11 +0600

    app text class added

---
+++ lib/utilities/app_text.dart
@@ -0,0 +1,6 @@
+class AppText{
+  static const String title = "Json Converter";
+  static const String fontRegular = "fontRegular";
+  static const String fontSemibold = "fontSemibold";
+  static const String welcome = "Welcome";
+}
