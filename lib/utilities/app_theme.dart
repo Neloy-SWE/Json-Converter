0f939eea33416025cea16dd8e24f29fc12521813
Taufiq Ahommed Neloy <taufiqneloy.swe@gmail.com>
2024-07-12 03:25:55 +0600

    theme added

---
+++ lib/utilities/app_theme.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:json_converter/utilities/app_color.dart';
+import 'package:json_converter/utilities/app_text.dart';
+
+class AppTheme {
+  static final set = ThemeData(
+    scaffoldBackgroundColor: AppColor.colorBackground,
+    appBarTheme: const AppBarTheme(
+      iconTheme: IconThemeData(color: AppColor.colorPrimary),
+      elevation: 0,
+      color: AppColor.colorBackground,
+      systemOverlayStyle: SystemUiOverlayStyle(
+        statusBarColor: Colors.transparent,
+        statusBarIconBrightness: Brightness.dark,
+      ),
+      centerTitle: true,
+      titleTextStyle: TextStyle(
+          color: AppColor.colorPrimary,
+          fontSize: 20,
+          fontFamily: AppText.fontSemibold),
+    ),
+    textTheme: const TextTheme(
+      bodyLarge: TextStyle(
+        fontFamily: AppText.fontRegular,
+        fontSize: 15,
+      ),
+    ),
+  );
+}
